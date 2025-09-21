c27d16f4249b9e056c6b8ab5508095e745909cd6
nettemhandu <antonetteliebe@icloud.com>
2025-09-21 21:26:06 +0200

    implemented abstract class and attributes

---
+++ pet_simulation/src/main/java/za/co/simulation/Pet.java
@@ -0,0 +1,31 @@
+package za.co.simulation;
+
+public abstract class Pet {
+    // Attributes of the abstract class
+    private String name;
+    private int hunger;
+    private int happiness;
+    private int energy;
+
+    // Constructor to initialize attributes
+    public Pet(String name, int hunger, int happiness, int energy) {
+        this.name = name;
+        this.hunger = hunger;
+        this.happiness = happiness;
+        this.energy = energy;
+    }
+
+    // Abstract method (must be implemented by concrete subclasses)
+    public abstract void feed();
+    public abstract void play();
+    public abstract void sleep();
+    public abstract void makeSound();
+
+    // Getters for private attributes
+
+    public String getName() {
+        return name;
+    }
+
+
+}
